1e41be38fed28f5d7e7ad76175460dd004653105
Mike Ball <mikedball@gmail.com>
2015-05-21 22:57:38 -0400

    remove menu require

--- js/main.js
+++ js/main.js
@@ -3,6 +3,4 @@ var path = require('path'),
     gui = require('nw.gui'),
     heading = require(path.join(cwd, '/js/heading.js'));
 
-gui.Window.get().menu = require(path.join(cwd, '/js/menu.js'))(gui);
-
 document.getElementById('heading').innerHTML = heading.getHeading();
